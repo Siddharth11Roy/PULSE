73aea97324b91d0263b6729bb26fe518b2866b20
Siddharth11Roy <155230297+Siddharth11Roy@users.noreply.github.com>
2025-05-18 11:55:39 +0530

    Update __init__.py

--- community-pulse/app/routes/__init__.py
+++ community-pulse/app/routes/__init__.py
@@ -1,14 +1,14 @@
 from flask import Blueprint
 
-from .auth import auth_bp
-from .events import events_bp
-from .admin import admin_bp
-from .public import public_bp
+# from .auth import auth_bp
+# from .events import events_bp
+# from .admin import admin_bp
+# from .public import public_bp
 from .home import home_bp
 
 def register_blueprints(app):
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(events_bp)
-    app.register_blueprint(admin_bp)
-    app.register_blueprint(public_bp)
+    # app.register_blueprint(auth_bp)
+    # app.register_blueprint(events_bp)
+    # app.register_blueprint(admin_bp)
+    # app.register_blueprint(public_bp)
     app.register_blueprint(home_bp)
