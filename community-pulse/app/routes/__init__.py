a44fdabf610e3407fa757bed758acd29ec5fd4df
Siddharth11Roy <155230297+Siddharth11Roy@users.noreply.github.com>
2025-05-18 11:53:52 +0530

    Update __init__.py

--- community-pulse/app/routes/__init__.py
+++ community-pulse/app/routes/__init__.py
@@ -1,8 +1,14 @@
-# route/home.py
-from flask import Blueprint, render_template
+from flask import Blueprint
 
-home_bp = Blueprint('home', __name__)
+from .auth import auth_bp
+from .events import events_bp
+from .admin import admin_bp
+from .public import public_bp
+from .home import home_bp
 
-@home_bp.route('/')
-def home():
-    return "Welcome to Home Page!"
+def register_blueprints(app):
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(events_bp)
+    app.register_blueprint(admin_bp)
+    app.register_blueprint(public_bp)
+    app.register_blueprint(home_bp)
